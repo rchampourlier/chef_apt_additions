a24ecc245251f88189e4cbe9033abffb51e2e6a9
Romain Champourlier <pro@rchampourlier.com>
2012-10-01 09:47:23 +0200

    Using 'apt-get upgrade -y' (added -y option)

--- recipes/upgrade_at_start.rb
+++ recipes/upgrade_at_start.rb
@@ -1,7 +1,7 @@
 include_recipe "apt_additions::update_at_start"
 
 execute "apt-get-upgrade-at-start" do
-  command "apt-get upgrade"
+  command "apt-get upgrade -y"
   ignore_failure true
   action :nothing
 end.run_action(:run)
